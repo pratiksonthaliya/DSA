c562d7aa75c4ed7135a0dc8b7a6c794d3bc37476
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-03-20 21:01:28 +0530

    Time: 13 ms (86.36%) | Memory: 204.7 MB (92.21%) - LeetSync

---
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -0,0 +1,56 @@
+class Solution {
+private:
+    vector<int> parent;
+    vector<int> depth;
+    int find(int node){
+        if(parent[node] == -1){
+            return node;
+        }
+
+        return parent[node] = find(parent[node]);
+    }
+
+    void Union(int node1, int node2){
+        int root1 = find(node1);
+        int root2 = find(node2);
+
+        if(root1 == root2) return;
+
+        if(depth[root1] < depth[root2]){
+            swap(root1, root2);
+        }
+
+        parent[root2] = root1;
+        if(depth[root1] == depth[root2]) depth[root1]++;
+    }
+public:
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
+        parent.resize(n, -1);
+        depth.resize(n, 0);
+
+        vector<unsigned int> costs(n, pow(2, 30) - 1);
+        for(auto &edge : edges){
+            Union(edge[0], edge[1]);
+        }
+
+        for(auto &edge : edges){
+            int root = find(edge[0]);
+            costs[root] &= edge[2];
+        }
+
+        vector<int> ans;
+        for(auto &q : queries){
+            int st = q[0];
+            int en = q[1];
+
+            if(find(st) != find(en)){
+                ans.push_back(-1);
+            } else {
+                int root = find(st);
+                ans.push_back(costs[root]);
+            }
+        }
+
+        return ans;
+    }
+};
