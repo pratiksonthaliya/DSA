ead9bd398c3470b325250c7e82e33e64cb27f18f
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-03-22 10:29:49 +0530

    Time: 88 ms (22.34%) | Memory: 146.5 MB (13.32%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -15,6 +15,26 @@ private:
             }
         }
     }
+
+    void bfs(int root, unordered_map<int, vector<int>> &mp, vector<bool> &vis, int &nodeCt, int &edgeCt){
+        queue<int> q;
+        q.push(root);
+        vis[root] = 1;
+
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+
+            // vis[node] = 1;
+            nodeCt += 1;
+            edgeCt += mp[node].size();
+            for(int nn : mp[node]){
+                if(!vis[nn]){
+                    vis[nn] = 1;
+                    q.push(nn);
+                }
+            }
+        }
+    }
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         unordered_map<int, vector<int>> mp;
@@ -31,7 +51,9 @@ public:
             }
             else if(!vis[i]){
                 int nodeCt = 0, edgeCt = 0;
-                dfs(i, mp, vis, nodeCt, edgeCt);
+
+                // dfs(i, mp, vis, nodeCt, edgeCt);
+                bfs(i, mp, vis, nodeCt, edgeCt);
 
                 if(edgeCt/2 == (nodeCt*(nodeCt-1))/2){
                     ct++;
