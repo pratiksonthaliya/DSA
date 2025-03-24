6bcfaa7c30202839b0e132c5640cae44e519effa
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-03-24 05:51:36 +0530

    Time: 81 ms (28.40%) | Memory: 126.3 MB (94.73%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -2,31 +2,19 @@ class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
-
-        // Merge
-        vector<vector<int>> merged;
-        merged.push_back(meetings[0]);
-        for(int i=1; i<meetings.size(); i++){
-            if(meetings[i][0] > merged.back()[1]){
-                merged.push_back(meetings[i]);
-            } else {
-                merged.back()[1] = max(merged.back()[1], meetings[i][1]);
-            }
-        }
         
-        int ans = 0;
-        for(int i=0; i<merged.size(); i++){
-            if(i == 0 && merged[0][0] > 1){
-                ans += (merged[0][0] - 1);
+        int ans = 0, latestEnd = 0;
+        for(int i=0; i<meetings.size(); i++){
+            int start = meetings[i][0];
+            int end = meetings[i][1];
+            
+            if(start > latestEnd + 1){
+                ans += (start - latestEnd - 1);
             }
-            if(i>0 && merged[i][0] > merged[i-1][1]){
-                ans += (merged[i][0] - merged[i-1][1] - 1);
-            }
-        }
-        if(merged[merged.size() - 1][1] < days){
-            ans += (days - merged[merged.size() - 1][1]);
+            latestEnd = max(latestEnd, end);
         }
 
+        ans += (days - latestEnd);
         return ans;
     }
 };
