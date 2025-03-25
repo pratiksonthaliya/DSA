3aaf9ca4eefe3e6128bc8c82f94dbcb47e8c6d0b
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-03-25 08:05:12 +0530

    Time: 127 ms (72.21%) | Memory: 185.4 MB (98.03%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
@@ -1,40 +1,26 @@
 class Solution {
-// private:
-//     bool
-public:
-    bool checkValidCuts(int n, vector<vector<int>>& rects) {
-        sort(rects.begin(), rects.end(), [](auto &a, auto &b) {
-            if(a[0] == b[0]) return a[2] < b[2];
-            return a[0] < b[0];
+private:
+    bool check(int st, int en, vector<vector<int>>& rects) {
+        sort(rects.begin(), rects.end(), [st, en](auto &a, auto &b) {
+            if(a[st] == b[st]) return a[en] < b[en];
+            return a[st] < b[st];
         });
 
         int endIdx = 0, ct = 0;
         for(auto &a : rects){
-            if(a[0] >= endIdx) {
+            if(a[st] >= endIdx) {
                 ct++;
                 if(ct == 3){
                     return true;
                 }
             }
-            endIdx = max(endIdx, a[2]);
-        }
-
-        sort(rects.begin(), rects.end(), [](auto &a, auto &b) {
-            if(a[1] == b[1]) return a[3] < b[3];
-            return a[1] < b[1];
-        });
-
-        endIdx = 0, ct = 0;
-        for(auto &a : rects){
-            if(a[1] >= endIdx) {
-                ct++;
-                if(ct == 3){
-                    return true;
-                }
-            }
-            endIdx = max(endIdx, a[3]);
+            endIdx = max(endIdx, a[en]);
         }
 
         return false;
+    }
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rects) {
+        return check(0, 2, rects) || check(1, 3, rects);
     }
 };
