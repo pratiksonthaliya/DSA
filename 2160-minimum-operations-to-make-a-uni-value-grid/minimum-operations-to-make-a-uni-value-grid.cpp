add759abc4e5f988c436d93b8745032f6d708347
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-03-26 10:39:51 +0530

    Time: 46 ms (46.20%) | Memory: 90 MB (48.02%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -22,21 +22,11 @@ public:
             }
         }    
 
-        if(nums.size() == 1) return 0;
+        int n = nums.size();
+        if(n == 1) return 0;
 
         sort(nums.begin(), nums.end());
-
-        long long avg = accumulate(nums.begin(), nums.end(), 0l) / nums.size();
-
-        int k1 = avg - avg%x;
-        int k2 = k1 + x;
-
-        int n = nums.size();
-
         int ans = getAns(nums[n/2], x, nums);
-        // int ct2 = getAns(nums[(n+1)/2], x, nums);
-
-        // int ans = min(ct1, ct2);
         return (ans == INT_MAX) ? -1 : ans;
     }
 };
