3fd3ffaf6f8d9c4a9ce3a88815dfa4ba3418a505
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-03-27 09:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 89.8 MB (85.66%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -2,22 +2,31 @@ class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int> mp;
-        for(int &num : nums) mp[num]++;
 
-        int maxVal = -1, maxFreq = 0;
-        for(auto [num, freq] : mp){
-            if(freq > maxFreq){
-                maxVal = num;
-                maxFreq = freq;
+        int maxVal = -1, maxCt = 0;
+        for(int i=0; i<n; i++){
+            if(maxCt == 0){
+                maxVal = nums[i];
+                maxCt = 1;
+            } else if(nums[i] == maxVal){
+                maxCt++;
+            } else {
+                maxCt--;
             }
         }
 
-        int len = 0;
+        maxCt = 0;
         for(int i=0; i<n; i++){
             if(nums[i] == maxVal){
-                len++;
-                if(2 * len > (i + 1) && 2 * (maxFreq - len) > n - (i + 1)){
+                maxCt++;
+            }
+        }
+
+        int ct = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == maxVal){
+                ct++;
+                if(2 * ct > (i + 1) && 2 * (maxCt - ct) > n - (i + 1)){
                     return i;
                 }
             }
