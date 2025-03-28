9c936e9862b4736ecd6723b8c84d0b85dbc1f462
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-03-28 10:29:57 +0530

    Time: 95 ms (68.12%) | Memory: 39.9 MB (86.23%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -1,42 +1,45 @@
+#define P pair<int, int>
 class Solution {
+private:
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, -1, 0, 1};
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        int rowCount = grid.size(), colCount = grid[0].size();
-        vector<int> result(queries.size(), 0);
-        vector<pair<int, int>> DIRECTIONS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-        vector<pair<int, int>> sortedQueries;
-        for (int index = 0; index < queries.size(); index++) {
-            sortedQueries.push_back({queries[index], index});
+        int n = grid.size(), m = grid[0].size(), k = queries.size();
+        vector<int> ans(k, 0);
+        
+        vector<P> sortedQueries;
+        for(int i=0; i<k; i++){
+            sortedQueries.push_back({queries[i], i});
         }
         sort(sortedQueries.begin(), sortedQueries.end());
 
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> minHeap;
-        vector<vector<bool>> visited(rowCount, vector<bool>(colCount, false));
-        int totalPoints = 0;
-        minHeap.push({grid[0][0], {0, 0}});
-        visited[0][0] = true;
+        priority_queue<pair<int, P>, vector<pair<int, P>>, greater<pair<int, P>> > pq;
+        vector<vector<bool>> vis(n, vector<bool> (m, 0));
+        pq.push({grid[0][0], {0, 0}});
+        vis[0][0] = 1;
 
-        for (auto [queryValue, queryIndex] : sortedQueries) {
-            while (!minHeap.empty() && minHeap.top().first < queryValue) {
-                auto [cellValue, position] = minHeap.top();
-                minHeap.pop();
-                int currentRow = position.first, currentCol = position.second;
-                totalPoints++;
+        int ct = 0;
+        for(auto &q : sortedQueries){
+            while(pq.size() > 0 && pq.top().first < q.first){
+                auto it = pq.top(); pq.pop();
+                
+                ct++;
 
-                for (auto [rowOffset, colOffset] : DIRECTIONS) {
-                    int newRow = currentRow + rowOffset,
-                        newCol = currentCol + colOffset;
+                for(int i=0; i<4; i++){
+                    int xx = dx[i] + it.second.first;
+                    int yy = dy[i] + it.second.second;
 
-                    if (newRow >= 0 && newCol >= 0 && newRow < rowCount &&
-                        newCol < colCount && !visited[newRow][newCol]) {
-                        minHeap.push({grid[newRow][newCol], {newRow, newCol}});
-                        visited[newRow][newCol] = true;
+                    if(xx >= 0 && yy >= 0 && xx < n && yy < m && !vis[xx][yy]){
+                        pq.push({grid[xx][yy], {xx, yy}});
+                        vis[xx][yy] = 1;
                     }
                 }
             }
-            result[queryIndex] = totalPoints;
+
+            ans[q.second] = ct;
         }
-        return result;
+
+        return ans;
     }
 };
