51030722b2ad9e2093e454e79c6d7fb9b8769511
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-04-07 11:41:37 +0530

    Time: 123 ms (55.22%) | Memory: 100.9 MB (24.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
-private:
-    bool canPartitionHelper(int i, long long tar, vector<int> &nums, vector<vector<int>> &dp){
-        if(tar == 0) return true;
-
-        if(i >= nums.size() || tar < 0) return false;
-
-        if(dp[i][tar] != -1) return dp[i][tar];
-
-        return dp[i][tar] = canPartitionHelper(i+1, tar - nums[i], nums, dp) || canPartitionHelper(i+1, tar, nums, dp);
-    }
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         long long sum = accumulate(nums.begin(), nums.end(), 0ll);
         if((sum & 1) == 1) return false;
 
-        vector<vector<int>> dp(n+1, vector<int> (sum/2 + 1, -1));
-        return canPartitionHelper(0, sum/2, nums, dp);
+        int tar = sum/2;
+        vector<vector<int>> dp(n+1, vector<int> (tar + 1, 0));
+        dp[n][0] = 1;
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<=tar; j++){
+                dp[i][j] = dp[i+1][j];
+                if(j - nums[i] >= 0){
+                    dp[i][j] |= dp[i+1][j - nums[i]];
+                } 
+            }
+        }
+
+        return dp[0][tar];
     }
 };
