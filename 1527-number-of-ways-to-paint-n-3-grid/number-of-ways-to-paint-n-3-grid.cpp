5be5e5394b7e8527e338bd131b71a3978953f176
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-05-19 22:21:23 +0530

    Time: 2434 ms (5.21%) | Memory: 104.3 MB (5.21%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,62 @@
+class Solution {
+private:
+    static constexpr int mod = 1000000007;
+    unordered_set<string> prevStates;
+    void getColumnStates(int i, int m, string s){
+        if(i == m){
+            prevStates.insert(s);
+            return;
+        }
+
+        if(i == 0){
+            for(char ch : {'R', 'G', 'B'}){
+                getColumnStates(i+1, m, s + ch);
+            }
+        } else {
+            for(char ch : {'R', 'G', 'B'}){
+                if(s.back() != ch)
+                    getColumnStates(i+1, m, s + ch);
+            }
+        }
+    }
+
+    bool isValid(string &t, string &s){
+        for(int i=0; i<t.size(); i++){
+            if(t[i] == s[i]) return false;
+        }
+        return true;
+    }
+    
+    unordered_map<string, int> dp;
+    int solve(string p, int i, int n){
+        if(i == n) return 1;
+
+        string key = p + "_" + to_string(i);
+
+        if(dp.find(key) != dp.end()){
+            return dp[key];
+        }
+
+        int ans = 0;
+        for(string t : prevStates){
+            if(isValid(t, p)){
+                ans = (ans + solve(t, i+1, n))%mod;
+            }
+        }
+
+        return dp[key] = ans;
+    }
+public:
+    int numOfWays(int n) {
+        int m = 3;
+        string t = "";
+        getColumnStates(0, m, t);
+
+        long res = 0;
+        for(string s : prevStates){
+            res = (res + solve(s, 1, n))%mod;
+        }
+
+        return res;
+    }
+};
