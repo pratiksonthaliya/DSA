b6d84f50d743b4a08f676ec2d4113e5c4005dec4
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-05-25 21:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (32.98%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -8,14 +8,12 @@ public:
         vector<int> dp(n+1, 0);
         dp[0] = dp[1] = 1;
 
-        for(int i=1; i<n; i++){
-            int one = (s[i] - '0');
-            int two = stoi(s.substr(i-1, 2));
-
-            if(one > 0 && one < 10){
+        for(int i=1; i<n; i++){            
+            if(s[i] != '0'){
                 dp[i+1] += dp[i];
             }
 
+            int two = stoi(s.substr(i-1, 2));
             if(two > 9 && two < 27 ){
                 dp[i+1] += dp[i-1];
             }
