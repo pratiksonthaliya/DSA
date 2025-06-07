8447b53796c3205baa8f8a3b72ff39cf6589550e
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-07 18:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (74.34%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n = intervals.size();
         vector<vector<int>> ans;
 
-        int i=0;
+        int i = 0, n = intervals.size();
         while(i < n && newInterval[0] > intervals[i][1]){ // non ovelapping
-            ans.push_back(intervals[i]);
-            i++;
+            ans.push_back(intervals[i++]);
         }
 
         while(i < n && newInterval[1] >= intervals[i][0]){ // overlapping case and merging
@@ -18,8 +16,7 @@ public:
         ans.push_back(newInterval);
 
         while(i < n){ // non ovelapping
-            ans.push_back(intervals[i]);
-            i++;
+            ans.push_back(intervals[i++]);
         }
 
         return ans;
