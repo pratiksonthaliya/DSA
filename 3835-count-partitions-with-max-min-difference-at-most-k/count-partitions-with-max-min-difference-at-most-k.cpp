a2803720a47597e14cc7f1b4f12097697b305a68
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-21 16:50:16 +0530

    Time: 227 ms (36.57%) | Memory: 232.3 MB (7.08%) - LeetSync

---
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int countPartitions(vector<int>& nums, int k) {
+    
+        // [[9]]         -> 1
+        // [[9], [4]]    -> 1
+        // [[9], [4, 1]], [[9], [4], [1]] -> 2
+        // [[9], [4, 1, 3]], [[9], [4], [1], [3]], [[9], [4], [1, 3]], [[9], [4, 1], [3]] -> 4
+        // [[9], [4, 1], [3, 7]], [[9], [4, 1, 3], [7]] -> 
+
+        int n = nums.size();
+        vector<long long> dp(n+1), pre(n+1, 0);
+        dp[0] = pre[0] = 1;
+
+        multiset<int> mp;
+        int i=0, j=0;
+
+        while(j < n){
+            mp.insert(nums[j]);
+            while(*(--mp.end()) - *mp.begin() > k){
+                mp.erase(mp.find(nums[i]));
+                i++;
+            }
+
+            // long long val = 0;
+            // for(int idx=i; idx<=j; idx++){
+            //     val += dp[idx];
+            // }
+
+            long long val = pre[j] - (i == 0 ? 0 : pre[i-1]);
+
+            dp[j+1] = val%MOD;
+            pre[j+1] = pre[j] + dp[j+1];
+            j++;
+        }
+
+        return dp[n];
+    }
+};
