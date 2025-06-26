bbe9310509440db076340fefb344e57521b2513f
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-26 12:40:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (50.79%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-        // int ct = 32 - __builtin_popcount(k);
 
         long int num = 0, i = s.size()-1, j = 0;
 
         int ct = 0;
         while(i >= 0 && (num|(1<<j)) <= k){
-
             if(s[i] == '1'){
                 num |= (1<<j);
             }
-
-            //cout << num << endl;
-
             i--; j++; ct++;
         }
-
-        //cout << ct << " " << i << " " << num << endl;
 
         for(int j=i; j>=0; j--){
             if(s[j] == '0') ct++;
         }
-
 
         return ct;
     }
