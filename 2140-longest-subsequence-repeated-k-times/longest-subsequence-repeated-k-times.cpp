8f298ed32dc9134f138c4fbba32b69ebfed16270
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-27 14:56:22 +0530

    Time: 126 ms (89.13%) | Memory: 11.8 MB (71.01%) - LeetSync

---
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    bool isKRepeatedSubsequence(string& t, int k, string &s){
+        int i = 0;
+        for(char ch : s){
+            if(ch == t[i]){
+                i++;
+                if(i == t.size()){
+                    i = 0;
+                    k--;
+                    if(k == 0) return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+public:
+    string longestSubsequenceRepeatedK(string s, int k) {
+        vector<int> freq(26);
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+        vector<char> candidate;
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] >= k) {
+                candidate.push_back('a' + i);
+            }
+        }
+
+        queue<string> q;
+        for(char ch : candidate){
+            q.push(string(1, ch));
+        }
+
+        string ans = "";
+        while(!q.empty()){
+            string curr = q.front();
+            q.pop();
+
+            if(curr.size() > ans.size()){
+                ans = curr;
+            }
+
+            for(char ch : candidate){
+                string next = curr + ch;
+                if(isKRepeatedSubsequence(next, k, s)){
+                    q.push(next);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
