698377b03b899969017cff6ed5d5566f147e0fd8
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-28 10:36:30 +0530

    Time: 1235 ms (85.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT customer_id, count_no_trans
+FROM (
+    SELECT customer_id, v.visit_id, SUM(CASE WHEN transaction_id IS NULL THEN 1 ELSE 0 END) AS count_no_trans
+    FROM Visits v
+    LEFT JOIN Transactions t
+    ON v.visit_id = t.visit_id
+    GROUP BY customer_id
+    ORDER BY customer_id
+) AS new_t WHERE count_no_trans > 0;
