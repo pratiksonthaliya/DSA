9a10c57c4caa751d32dd5c3a83fd33079f23a223
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-28 10:50:39 +0530

    Time: 366 ms (52.60%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -3,7 +3,6 @@
 SELECT DISTINCT
     query_name, 
     ROUND(AVG(rating/position), 2) AS quality,
-    -- SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)
-    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(rating) * 100, 2) AS poor_query_percentage
+    ROUND(SUM(rating < 3) / COUNT(rating) * 100, 2) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name
