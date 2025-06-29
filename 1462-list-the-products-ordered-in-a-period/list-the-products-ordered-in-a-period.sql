d46c35ac67ab1f3f36f3e09b5a54e5d306e4ac6c
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-29 14:39:50 +0530

    Time: 680 ms (81.54%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,13 +1,11 @@
 # Write your MySQL query statement below
 
-SELECT p.product_name, t.units AS unit
-FROM
-
-((SELECT DISTINCT o.product_id, o.order_date, SUM(CASE WHEN DATE_FORMAT(o.order_date, '%Y-%m') = '2020-02' THEN unit ELSE 0 END) AS units
-FROM Orders o
+SELECT 
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM Orders AS o
+LEFT JOIN Products AS p
+    ON o.product_id = p.product_id
+WHERE DATE_FORMAT(o.order_date, '%Y-%m') = '2020-02'
 GROUP BY o.product_id
-HAVING units >= 100
-) AS t
-
-LEFT JOIN Products p
-ON t.product_id = p.product_id);
+HAVING unit >= 100;
