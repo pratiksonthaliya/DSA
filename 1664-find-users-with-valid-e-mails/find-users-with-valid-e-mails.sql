ee2c5832c89c282aacb963141c2ad8dd82fbd800
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-30 09:23:54 +0530

    Time: 652 ms (84.19%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,4 +1,6 @@
 SELECT user_id, name, mail
 FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$'
-and mail like '%@leetcode.com' COLLATE utf8mb4_bin;
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'
+    AND mail LIKE '%@leetcode.com'
+    AND BINARY RIGHT(mail, 13) = '@leetcode.com'
