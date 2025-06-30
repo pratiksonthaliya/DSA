1b5814d48cd491afc50828a8b01aa0e29c1740be
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-06-30 09:51:20 +0530

    Time: 592 ms (29.04%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,12 +1,6 @@
 -- Employees with only one department
 SELECT employee_id, department_id
 FROM Employee
-GROUP BY employee_id
-HAVING COUNT(department_id) = 1
-
-UNION
-
--- Employees with multiple departments and primary flag
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
+WHERE 
+    primary_flag = 'Y' OR
+    employee_id IN (SELECT employee_id FROM Employee GROUP BY employee_id HAVING COUNT(department_id) = 1)
