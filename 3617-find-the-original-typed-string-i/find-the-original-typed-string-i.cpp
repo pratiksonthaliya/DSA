473d8c2c21dd2a676ed5e620ec53ca5aa04972d1
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-01 09:06:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (29.69%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        
-        int ct = 0, freq = 0;
-        int i = 0, j = 0, n = word.size();
-        while(i < n && j < n){
-            while(j < n && word[i] == word[j]){
-                j++;
+        int ans = 1;
+        for(int i=1; i<word.size(); i++){
+            if(word[i-1] == word[i]){
+                ans++;
             }
-
-            //cout << i << " " << j << endl;
-
-            if(j - i > 1){
-                ct++;
-                freq += (j - i);
-            }
-
-            i = j;
         }
-
-        if(ct == 0) return 1;
-        return freq - (ct - 1);
+        return ans;
     }
 };
