1b88f50b306aa59584aeb0f7190bb5ae324115d6
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-02 09:36:56 +0530

    Time: 746 ms (81.81%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,24 +1,13 @@
+# Write your MySQL query statement below
+
 SELECT 
     p.product_id,
-    IFNULL(
-        ROUND(
-            SUM(
-                CASE 
-                    WHEN s.purchase_date BETWEEN p.start_date AND p.end_date THEN s.units * p.price
-                    ELSE 0
-                END
-            ) / NULLIF(
-                SUM(
-                    CASE
-                        WHEN s.purchase_date BETWEEN p.start_date AND p.end_date THEN s.units
-                        ELSE 0
-                    END
-                ), 0
-            ),
-            2
-        ), 
-    0) AS average_price
+    CASE 
+        WHEN SUM(s.units) IS NULL THEN 0
+        WHEN SUM(s.units) = 0 THEN 0
+        ELSE ROUND(SUM(s.units * p.price) / SUM(s.units), 2)
+    END AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold s
-    ON p.product_id = s.product_id
-GROUP BY p.product_id;
+ON p.product_id = s.product_id AND s.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
