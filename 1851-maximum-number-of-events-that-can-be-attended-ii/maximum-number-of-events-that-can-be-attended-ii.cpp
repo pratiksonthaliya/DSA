ad8adac47050537d314bb4ced944f4e9166122ba
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-08 09:29:59 +0530

    Time: 65 ms (99.29%) | Memory: 111.3 MB (95.77%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,43 +1,21 @@
 class Solution {
 private:
     vector<vector<int>> dp;
-    int findNextIdx(int val, vector<vector<int>>& events){
 
-        int st = 0, en = events.size() - 1, idx = en + 1;
-        while(st <= en){
-
-            int mid = st + (en - st)/2;
-            if(events[mid][0] > val){
-                idx = mid;
-                en = mid - 1;
-            } else {
-                st = mid + 1;
-            }
-        }
-
-        return idx;
-    }
-    int solve(int idx, int k, vector<vector<int>>& events){
+    int solve(int idx, int k, int prev_end, vector<vector<int>>& events){
         if(idx == events.size() || k == 0){
             return 0;
         }
 
+        if(prev_end >= events[idx][0]){
+            return solve(idx + 1, k, prev_end, events);;
+        }
+
         if(dp[k][idx] != -1) return dp[k][idx];
 
-        int next_idx = findNextIdx(events[idx][1], events);
-
-        // // skip
-        // int skip = solve(idx + 1, k, prev_end, events);
-
-        // //pick
-        // int pick = INT_MIN;
-        // if(prev_end < events[idx][0] && k > 0){
-        //     pick = events[idx][2] + solve(next_idx, k-1, events[idx][1], events);
-        // }
-
         return dp[k][idx] = max(
-            solve(idx + 1, k, events), 
-            events[idx][2] + solve(next_idx, k-1, events)
+            solve(idx + 1, k, prev_end, events), 
+            events[idx][2] + solve(idx + 1, k-1, events[idx][1], events)
         );
     }
 public:
@@ -51,6 +29,6 @@ public:
 
         dp.resize(k+1, vector<int> (events.size()+1, -1));
 
-        return solve(0, k, events);
+        return solve(0, k, 0, events);
     }   
 };
