24d2b155323128b5e6a51389f3f426d37bec429a
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-11 18:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.00%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,14 +4,9 @@ public:
         int st = 0, en = nums.size() - 1;
         while(st <= en){
             int mid = st + (en - st)/2;
-
-            cout << mid << " ";
-
             if(nums[mid] == target){
                 return mid;
-            }
-
-            if(nums[mid] >= nums[0]){
+            } else if(nums[mid] >= nums[0]){
                 if(nums[0] <= target && nums[mid] >= target){
                     en = mid - 1;
                 } else {
