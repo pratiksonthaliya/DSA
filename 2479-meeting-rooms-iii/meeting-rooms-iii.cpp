f9552f06e77b9f91a4d31bb76a01a398fd81cf58
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-11 21:57:49 +0530

    Time: 131 ms (27.39%) | Memory: 111.2 MB (29.58%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,53 +1,46 @@
 class Solution {
 public:
-    int mostBooked(int n, vector<vector<int>>& meetings) {
+    int mostBooked(int n, vector<vector<int>>& meetings) {   
         sort(meetings.begin(), meetings.end());
+
+        vector<int> meetingsHeld(n, 0);
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<>> used; // {end_time, room_no}
         
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> engaged;
-        priority_queue<int, vector<int>, greater<int>> unused;
-        
-        unordered_map<int, int> freq;
-        
-        for(int i = 0; i < n; i++) {
-            unused.push(i);
-        }
-        
-        for(auto x : meetings) {
-            int s = x[0], e = x[1];
-            
-            while(engaged.size() > 0 && engaged.top().first <= s) {
-                int room = engaged.top().second;
-                engaged.pop();
-                
-                unused.push(room);
+        priority_queue<long long, vector<long long>, greater<>> unused; 
+        for(long long i=0; i<n; i++) unused.push(i);
+
+        for(auto meeting : meetings){
+            long long st_t = meeting[0], en_t = meeting[1];
+
+            while(!used.empty() && used.top().first <= st_t){ // jo empty ho rha h usko empty krdo
+                unused.push(used.top().second);
+                used.pop();
             }
-            
-            if(unused.size() > 0) {
-                int room = unused.top();
+
+            // unused hai
+            if(!unused.empty()){
+                long long room_no = unused.top();
+                used.push({en_t, room_no});
                 unused.pop();
-                
-                freq[abs(room)] += 1;
-                
-                engaged.push({e, room});
-            }
-            else {
-                pair<long long, int> topmost = engaged.top();
-                engaged.pop();
-            
-                freq[abs(topmost.second)] += 1;
-                
-                long long newEnd = topmost.first;
-                newEnd += (e - s);
-                
-                engaged.push({newEnd, topmost.second});
+                meetingsHeld[room_no]++;
+            } else {
+                long long time = used.top().first;
+                long long room_no = used.top().second;
+                used.pop();
+
+                used.push({en_t - st_t + time, room_no});
+                meetingsHeld[room_no]++;
             }
         }
-        
-        int maxi = 0;
-        for(int i = 1; i < n; i++) {
-            if(freq[i] > freq[maxi])
-                maxi = i;
+
+        int ans = 0, max_val = 0;
+        for(int i=0; i<n; i++){
+            //cout << meetingsHeld[i] << " ";
+            if(meetingsHeld[i] > max_val){
+                ans = max(ans, i);
+                max_val = meetingsHeld[i];
+            }
         }
-        return maxi;
+        return ans;
     }
 };
