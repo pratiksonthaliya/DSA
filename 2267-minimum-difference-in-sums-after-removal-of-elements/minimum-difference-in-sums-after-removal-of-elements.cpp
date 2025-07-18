bdba67713157a40b45a0582c63ee903d9c31a2e2
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-18 18:28:45 +0530

    Time: 268 ms (21.39%) | Memory: 154.6 MB (64.95%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -3,41 +3,32 @@ public:
     long long minimumDifference(vector<int>& nums) {
         int n = nums.size()/3;
 
-        vector<long long> preMinSum(3*n, 0), sufMaxSum(3*n, 0);
+        vector<long long> preMinSum(3*n, 0);
         priority_queue<int> mxPQ;
-        priority_queue<int, vector<int>, greater<>> mnPQ;
-
         long long sum1 = 0, sum2 = 0;
         for(int i=0; i<3*n; i++){
             mxPQ.push(nums[i]);
-            mnPQ.push(nums[3*n-1-i]);
-
             sum1 += nums[i];
-            sum2 += nums[3*n-1-i];
 
             while(mxPQ.size() > n){
                 sum1 -= mxPQ.top();
                 mxPQ.pop();
             }
+            preMinSum[i] = sum1;
+        }
+
+        priority_queue<int, vector<int>, greater<>> mnPQ;
+        vector<long long> sufMaxSum(3*n, 0);
+        for(int i=3*n-1; i>=0; i--){
+            mnPQ.push(nums[i]);
+            sum2 += nums[i];
 
             while(mnPQ.size() > n){
                 sum2 -= mnPQ.top();
                 mnPQ.pop();
             }
-
-            preMinSum[i] = sum1;
-            sufMaxSum[3*n-1-i] = sum2;
-            // cout << sum << " ";
+            sufMaxSum[i] = sum2;
         }
-
-        // for(int i=0; i<3*n; i++){
-        //     cout << preMinSum[i] << " ";
-        // }
-        // cout << endl;
-
-        // for(int i=0; i<3*n; i++){
-        //     cout << sufMaxSum[i] << " ";
-        // }
 
         long long ans = LLONG_MAX;
         for(int i=n-1; i<2*n; i++){
