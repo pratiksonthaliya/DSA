217a577771627cce309d9a4c2cd4289c4b275bfb
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-21 22:53:48 +0530

    Time: 27 ms (82.66%) | Memory: 90.7 MB (50.91%) - LeetSync

--- 3823-count-islands-with-total-value-divisible-by-k/count-islands-with-total-value-divisible-by-k.cpp
+++ 3823-count-islands-with-total-value-divisible-by-k/count-islands-with-total-value-divisible-by-k.cpp
@@ -2,22 +2,22 @@ class Solution {
 private:
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, -1, 0, 1};
-    void dfs(int i, int j, long long &sum, vector<vector<int>> &vis, vector<vector<int>>& grid){
+    long long dfs(int i, int j, vector<vector<int>> &vis, vector<vector<int>>& grid){
 
-        // long long sum = 0;
+        long long sum = grid[i][j];
         vis[i][j]=1;
-        sum += grid[i][j];
+        // sum += grid[i][j];
         
         for(int k=0; k<4; k++){
             int x = i+dx[k];
             int y = j+dy[k];
             if(x>=0 && y>=0 && x<grid.size() && y<grid[0].size() 
                    && grid[x][y] != 0 && !vis[x][y]){
-                dfs(x, y, sum, vis, grid);
+                sum += dfs(x, y, vis, grid);
             }
         }
 
-        // return sum;
+        return sum;
     }
 public:
     int countIslands(vector<vector<int>>& grid, int k) {
@@ -30,8 +30,7 @@ public:
             for(int j=0; j<m; j++){
                 if(!vis[i][j] && grid[i][j] != 0){
                     
-                    long long val = 0;
-                    dfs(i, j, val, vis, grid);
+                    long long val = dfs(i, j, vis, grid);
 
                     if(val%k == 0) ct++;
                 }
