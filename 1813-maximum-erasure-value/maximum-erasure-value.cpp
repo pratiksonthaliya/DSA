21805ff97ae950d047a942e73e457e6f5f179b48
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-22 09:15:52 +0530

    Time: 211 ms (18.22%) | Memory: 131.1 MB (31.90%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -3,22 +3,25 @@ public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int n = nums.size();
 
-        vector<long long> pre(n+1, 0);
-        for(int i=0; i<n; i++){
-            pre[i+1] = pre[i] + nums[i];
-        }
+        // vector<long long> pre(n+1, 0);
+        // for(int i=0; i<n; i++){
+        //     pre[i+1] = pre[i] + nums[i];
+        // }
 
         int i=0, j=0;
-        long long ans = 0;
+        long long ans = 0, sum = 0;
         unordered_set<int> st;
         while(i<n && j<n){
             while(st.count(nums[j])){
+                sum -= nums[i];
                 st.erase(nums[i]);
                 i++;
             }
 
             st.insert(nums[j]);
-            ans = max(ans, pre[j+1] - pre[i]);
+            sum += nums[j];
+            ans = max(ans, sum);
+            // ans = max(ans, pre[j+1] - pre[i]);
             j++;
         }
         return ans;
