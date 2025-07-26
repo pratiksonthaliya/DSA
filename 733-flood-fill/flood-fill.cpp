32f13683593ea20a5f1b6f19be58e1ab9a01d2cd
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-07-26 12:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (90.22%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,26 +1,20 @@
 class Solution {
 private:
-    int n, m;
     int dr[4] = {1, 0, -1, 0};
     int dc[4] = {0, 1, 0, -1};
     void fill(int r, int c, int stColor, int color, vector<vector<int>>& image){
-        if(r < 0 || c < 0 || r >= n || c >= m || image[r][c] != stColor || image[r][c] == color) return;
-
-        //cout << stColor << endl;
+        if(r < 0 || c < 0 || r >= image.size() || c >= image[0].size() 
+            || image[r][c] != stColor) return;
 
         image[r][c] = color;
-
         for(int k=0; k<4; k++){
-            int nr = r + dr[k];
-            int nc = c + dc[k];
-
-            fill(nr, nc, stColor, color, image);
+            fill(r + dr[k], c + dc[k], stColor, color, image);
         }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        n = image.size(), m = image[0].size();
-        fill(sr, sc, image[sr][sc], color, image);
+        if(image[sr][sc] != color)
+            fill(sr, sc, image[sr][sc], color, image);
         return image;
     }
 };
