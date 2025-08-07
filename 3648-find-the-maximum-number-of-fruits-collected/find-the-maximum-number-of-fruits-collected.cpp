dbb31ee755fc6fe4c2e8dbad106e36b32e14fe76
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-07 22:10:03 +0530

    Time: 207 ms (25.32%) | Memory: 193.4 MB (10.39%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -1,40 +1,52 @@
 class Solution {
+private:
+    int n;
+    vector<vector<int>> dp;
+    int solve1(int i, int j, vector<vector<int>>& fruits){
+        if(i == n-1 && j == n-1) return 0;
+        if(i > n-1 || j > n-1 || i == j) return -1e9;
+        // if(i >= n/2 && j < (n+1)/2) return -1e9;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        return dp[i][j] = fruits[i][j] + max({solve1(i+1, j-1, fruits), solve1(i+1, j, fruits), solve1(i+1, j+1, fruits)}); 
+    }
+    int solve2(int i, int j, vector<vector<int>>& fruits){
+        if(i == n-1 && j == n-1) return 0;
+        if(i > n-1 || j > n-1 || (i == j)) return -1e9;
+        // if(i < (n+1)/2 && j >= n/2) return -1e9;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        return dp[i][j] = fruits[i][j] + max({solve2(i-1, j+1, fruits), solve2(i, j+1, fruits), solve2(i+1, j+1, fruits)}); 
+    }
+    // int solve3(int i, int j, vector<vector<int>>& fruits){
+    //     if(i == (n+1)/2 - 1 && j == (n+1)/2 - 1) return 0;
+    //     if(i >= (n+1)/2 || j >= (n+1)/2) return -1e9;
+
+    //     return fruits[i][j] + max({solve3(i, j+1, fruits), solve3(i+1, j+1, fruits), solve3(i+1, j, fruits)}); 
+    // }
 public:
     int maxCollectedFruits(vector<vector<int>>& fruits) {
-        int n = fruits.size();
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            ans += fruits[i][i];
+        n = fruits.size();
+
+        vector<vector<int>> dp1(n, vector<int>(n, -1));
+        dp = dp1;
+        int f1 = solve1(0, n-1, fruits);
+
+        vector<vector<int>> dp2(n, vector<int>(n, -1));
+        dp = dp2;
+        int f2 = solve2(n-1, 0, fruits);
+
+        //int f3 = solve3(0, 0, fruits);
+
+        int f3 = 0;
+        for(int i=0; i<n; i++){
+            f3 += fruits[i][i];
         }
 
-        auto dp = [&]() -> int {
-            vector<int> prev(n, INT_MIN), curr(n, INT_MIN);
-            prev[n - 1] = fruits[0][n - 1];
-            for (int i = 1; i < n - 1; ++i) {
-                for (int j = max(n - 1 - i, i + 1); j < n; ++j) {
-                    int best = prev[j];
-                    if (j - 1 >= 0) {
-                        best = max(best, prev[j - 1]);
-                    }
-                    if (j + 1 < n) {
-                        best = max(best, prev[j + 1]);
-                    }
-                    curr[j] = best + fruits[i][j];
-                }
-                swap(prev, curr);
-            }
-            return prev[n - 1];
-        };
+        cout << f1 << " " << f2 << " " << f3 << endl;
 
-        ans += dp();
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                swap(fruits[j][i], fruits[i][j]);
-            }
-        }
-
-        ans += dp();
-        return ans;
+        return f1 + f2 + f3;
     }
 };
