16bf4174784fae1f507cea37f2240829def06c10
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-07 22:13:48 +0530

    Time: 204 ms (26.62%) | Memory: 175.3 MB (15.58%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -9,14 +9,14 @@ private:
         
         return dp[i][j] = fruits[i][j] + max({solve1(i+1, j-1, fruits, dp), solve1(i+1, j, fruits, dp), solve1(i+1, j+1, fruits, dp)}); 
     }
-    int solve2(int i, int j, vector<vector<int>>& fruits, vector<vector<int>>& dp){
-        if(i == n-1 && j == n-1) return 0;
-        if(i > n-1 || j > n-1 || (i == j)) return -1e9;
+    // int solve2(int i, int j, vector<vector<int>>& fruits, vector<vector<int>>& dp){
+    //     if(i == n-1 && j == n-1) return 0;
+    //     if(i > n-1 || j > n-1 || (i == j)) return -1e9;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
         
-        return dp[i][j] = fruits[i][j] + max({solve2(i-1, j+1, fruits, dp), solve2(i, j+1, fruits, dp), solve2(i+1, j+1, fruits, dp)}); 
-    }
+    //     return dp[i][j] = fruits[i][j] + max({solve2(i-1, j+1, fruits, dp), solve2(i, j+1, fruits, dp), solve2(i+1, j+1, fruits, dp)}); 
+    // }
 public:
     int maxCollectedFruits(vector<vector<int>>& fruits) {
         n = fruits.size();
@@ -24,8 +24,14 @@ public:
         vector<vector<int>> dp1(n, vector<int>(n, -1));
         int f1 = solve1(0, n-1, fruits, dp1);
 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                swap(fruits[i][j], fruits[j][i]);
+            }
+        }
+
         vector<vector<int>> dp2(n, vector<int>(n, -1));
-        int f2 = solve2(n-1, 0, fruits, dp2);
+        int f2 = solve1(0, n-1, fruits, dp2);
 
         int f3 = 0;
         for(int i=0; i<n; i++){
