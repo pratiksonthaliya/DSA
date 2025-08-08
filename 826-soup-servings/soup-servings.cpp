90b7b88606ee48f1c2dc0f9a705c9872744d5459
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-08 09:35:58 +0530

    Time: 143 ms (8.50%) | Memory: 432.3 MB (5.17%) - LeetSync

---
+++ 826-soup-servings/soup-servings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    vector<vector<int>> dirs = {{100, 0}, {75, 25}, {50, 50}, {25, 75}};
+    double solve(int a, int b, vector<vector<double>>& dp){
+        if(a <= 0 && b <= 0) return 0.5;
+        if(a <= 0) return 1.0;
+        if(b <= 0) return 0.0;
+
+        if(dp[a][b] != -1) return dp[a][b];
+
+        double ans = 0.0;
+        for(auto dir : dirs){
+            ans += solve(a - dir[0], b - dir[1], dp);
+        }
+
+        return dp[a][b] = ans/4;
+    }
+public:
+    double soupServings(int n) {
+        if(n > 4800) return 1.0;
+        vector<vector<double>> dp(n+1, vector<double> (n+1, -1));
+        return solve(n, n, dp);
+    }
+};
