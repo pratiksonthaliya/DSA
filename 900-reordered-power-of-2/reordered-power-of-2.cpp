b883fe270f286191a09c2aaecbd96ea6c139d9cf
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-10 17:52:24 +0530

    Time: 471 ms (5.01%) | Memory: 9 MB (62.83%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,34 +1,25 @@
 class Solution {
 private:
-    bool isPowOf2(string &s, string &t, vector<int> &vis){
-        if(t.size() == s.size()){
-            if(t[0] == '0') return 0;
+    bool isPowOf2(int idx, string &s){
+        if(idx == s.size()){
+            if(s[0] == '0') return 0;
 
-            int n = stoi(t);
+            int n = stoi(s);
             //cout << n << " ";
             return !(n & (n-1));
         }
 
         bool isPow = false;
-        for(int i=0; i<s.size(); i++){
-            if(vis[i]) continue;
-
-            vis[i] = 1;
-            t.push_back(s[i]);
-
-            isPow |= isPowOf2(s, t, vis);
-
-            t.pop_back();
-            vis[i] = 0;
-        } 
+        for(int i=idx; i<s.size(); i++){
+            swap(s[i], s[idx]);
+            isPow |= isPowOf2(idx+1, s);
+            swap(s[i], s[idx]);
+        }   
         return isPow;
     }
 public:
     bool reorderedPowerOf2(int n) {
         string s = to_string(n);
-        string t;
-        vector<int> vis(s.size()+1, 0);
-        
-        return isPowOf2(s, t, vis);
+        return isPowOf2(0, s);
     }
 };
