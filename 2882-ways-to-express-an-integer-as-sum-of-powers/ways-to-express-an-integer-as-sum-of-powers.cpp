2e53f81d4dc7e0b1b483f5da6d74be52cb475042
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-12 09:55:53 +0530

    Time: 267 ms (42.70%) | Memory: 274.6 MB (18.08%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -11,18 +11,27 @@ private:
             long int num = 1;
             for(int j=1; j<=x; j++) num *= i; 
 
-            //cout << num << " ";
-
             if(num <= n)
                 ways = (ways + find(i+1, n - num, x, dp))%MOD;
         }
-        //cout << endl;
 
         return dp[idx][n] = ways;
     }
 public:
     int numberOfWays(int n, int x) {
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        return find(1, n, x, dp);
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+
+        dp[0][0] = 1;
+        for(int i=1; i<=n; i++){
+            long long val = pow(i, x);
+            for(int j=0; j<=n; j++){
+                dp[i][j] = dp[i-1][j];
+                if(j >= val){
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-val])%MOD;
+                }
+            }
+        }
+
+        return dp[n][n];
     }
 };
