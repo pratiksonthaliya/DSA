10ca0d08281a5c4a96af6024e40b4818f3db44b5
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-13 09:36:53 +0530

    Time: 99 ms (5.68%) | Memory: 9.2 MB (5.34%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -3,11 +3,12 @@ public:
     bool isPowerOfThree(int n) {
         if(n <= 0) return 0;
 
-        long int num = 1;
-        while(num <= n){
-            if(num == n) return 1;
-            num = num * 3;
-        }
-        return 0;
+        double x = 1.0*log10(n)/log10(3);
+        // cout << 1.0*log(n)/log(3) << " " << (int) (1.0*log(n)/log(3)) << endl;
+        cout << x << " " << int(x) << endl;
+
+        return (x == (int) x);
+
+        return abs((log(n) + log(3) - 1)/log(3) - log(n)/log(3)) < 1;
     }
 };
