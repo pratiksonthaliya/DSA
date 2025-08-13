0837c8124b356adb6f9ba0fe04e5b0c6c644c959
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-13 09:24:18 +0530

    Time: 8 ms (16.56%) | Memory: 9 MB (46.41%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0) return 0;
+
+        long int num = 1;
+        while(num < INT_MAX){
+            if(num == n) return 1;
+            num = num * 3;
+        }
+        return 0;
+    }
+};
