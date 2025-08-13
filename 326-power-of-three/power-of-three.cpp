a7a276e97e3505752928bbda91b329717861d557
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-13 09:24:46 +0530

    Time: 15 ms (5.68%) | Memory: 9 MB (14.61%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -4,7 +4,7 @@ public:
         if(n <= 0) return 0;
 
         long int num = 1;
-        while(num < INT_MAX){
+        while(num <= n){
             if(num == n) return 1;
             num = num * 3;
         }
