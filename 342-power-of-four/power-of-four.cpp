f8be85331b8f7c9b5f5668893310282f7afa9c34
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-15 07:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.14%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == INT_MIN) return false;
-        return !(n&(n-1)) && (n-1)%3==0;
+        if(n <= 0) return 0;
+        double k = log10(n) / log10(4);
+        return k == (int) k;
     }
 };
