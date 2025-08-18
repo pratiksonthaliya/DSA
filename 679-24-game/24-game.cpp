cd757b6bc136c33f33457344ef1580032deccfc5
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-18 22:47:04 +0530

    Time: 9 ms (77.75%) | Memory: 14.6 MB (61.45%) - LeetSync

--- 679-24-game/24-game.cpp
+++ 679-24-game/24-game.cpp
@@ -1,37 +1,38 @@
 class Solution {
-public:
-    bool judgePoint24(vector<int>& cards) {
-        vector<double> nums(cards.begin(), cards.end());
-        return solve(nums);
-    }
-
 private:
-    bool solve(vector<double>& nums) {
-        if (nums.size() == 1) {
-            return fabs(nums[0] - 24.0) < 1e-6;
+    const double EPS = 1e-6;
+    bool dfs(vector<double>& nums){
+        if(nums.size() == 1){
+            return fabs(nums[0] - 24.0) < EPS;
         }
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+
                 if (i == j) continue;
 
                 vector<double> next;
-                for (int k = 0; k < n; k++) {
+                for (int k = 0; k < nums.size(); k++) {
                     if (k != i && k != j) next.push_back(nums[k]);
                 }
-
+                
                 double a = nums[i], b = nums[j];
-                vector<double> results = {a + b, a - b, b - a, a * b};
-                if (fabs(b) > 1e-6) results.push_back(a / b);
-                if (fabs(a) > 1e-6) results.push_back(b / a);
+                vector<double> res = {a+b, a-b, b-a, a*b};
+                if(fabs(b) > EPS) res.push_back(a/b);
+                if(fabs(a) > EPS) res.push_back(b/a);
 
-                for (double val : results) {
+                for(double val : res){
                     next.push_back(val);
-                    if (solve(next)) return true;
+                    if(dfs(next)) return true;
                     next.pop_back();
                 }
             }
         }
         return false;
+    }
+public:
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> nums(cards.begin(), cards.end());
+        return dfs(nums);
     }
 };
