4e3bbe8673daba7bc7747829832feaf7ada03891
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-20 09:20:47 +0530

    Time: 7 ms (54.96%) | Memory: 27.4 MB (96.05%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -3,7 +3,7 @@ public:
     int countSquares(vector<vector<int>>& matrix) {
         for(int i=1; i<matrix.size(); i++){
             for(int j=1; j<matrix[0].size(); j++){
-                if(matrix[i][j-1] > 0 && matrix[i-1][j] > 0 && matrix[i-1][j-1] > 0 && matrix[i][j] > 0){
+                if(matrix[i][j] > 0){
                     matrix[i][j] = 1 + min({matrix[i][j-1], matrix[i-1][j], matrix[i-1][j-1]});
                 }
             }
