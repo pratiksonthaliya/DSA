1b3ebd35f1d04f66cfc5755074866adc043bc62f
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-23 19:07:47 +0530

    Time: 8 ms (59.41%) | Memory: 19.5 MB (60.41%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -10,11 +10,8 @@ public:
                     dp_row[i][j] = mat[i][j];
                     if(i > 0) dp_row[i][j] += dp_row[i-1][j];
                 }
-                cout << dp_row[i][j] << " ";
             }
-            cout << endl;
         }
-        cout << endl;
 
         int ans = 0;
         for(int i=0; i<n; i++){
@@ -39,31 +36,7 @@ public:
                     else break;
                 }
             }
-
-            //cout << "Row " + i << " : " << ans << endl;
         }
-
-        // vector<vector<int>> dp_col(n, vector<int> (m, 0));
-        // for(int j=0; j<m; j++){
-        //     for(int i=0; i<n; i++){
-        //         if(mat[i][j] == 1){
-        //             dp_col[i][j] = mat[i][j];
-        //             if(j > 0) dp_col[i][j] += dp_col[i][j-1];
-        //         }
-        //     }
-        // }
-
-        // int ans = 0;
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<m; j++){
-        //         cout << dp_col[i][j] << " ";
-        //         if(mat[i][j] > 0){
-        //             ans += dp_row[i][j] + dp_col[i][j] - 1;
-        //             if(i > 0 && j > 0 && mat[i-1][j-1] == 1 && mat[i-1][j] == 1 && mat[i][j-1] == 1) ans++;
-        //         }
-        //     }
-        //     cout << endl;
-        // }
 
         return ans;
     }
