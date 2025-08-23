c9fe94627b144db275dc23f51c725404463c97aa
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-23 20:14:37 +0530

    Time: 3 ms (61.04%) | Memory: 12.7 MB (40.70%) - LeetSync

--- 867-new-21-game/new-21-game.cpp
+++ 867-new-21-game/new-21-game.cpp
@@ -6,21 +6,19 @@ public:
             return 1.0;
         }
 
-        vector<double> dp(n + 1);
+        vector<double> dp(n + 1, 0.0);
         double currSum = 1.0;
-        double ans = 0.0;
         dp[0] = 1.0;
 
-        for (int i = 1; i < n+1; i++) {
+        for (int i = 1; i <= n; i++) {
             dp[i] = currSum / maxPts;
 
-            if (i < k) currSum += dp[i];
-            else ans += dp[i];
+            if (i < k) 
+                currSum += dp[i];
             
-            if (i - maxPts >= 0) 
+            if (i - maxPts >= 0 && i - maxPts < k) 
                 currSum -= dp[i - maxPts];
         }
-
-        return ans;
+        return accumulate(dp.begin() + k, dp.end(), 0.0);
     }
 };
