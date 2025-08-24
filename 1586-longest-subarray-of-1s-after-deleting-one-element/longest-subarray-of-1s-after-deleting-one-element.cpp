134856202edd83eac49f26ea8264d1ec40b354fb
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-24 09:34:32 +0530

    Time: 7 ms (7.51%) | Memory: 65.7 MB (5.09%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -6,18 +6,16 @@ public:
         vector<int> pre(n, 0), suf(n, 0);
 
         pre[0] = nums[0];
+        suf[n-1] = nums[n-1];
+
         for(int i=1; i<n; i++){
             if(nums[i] == 1)
                 pre[i] = pre[i-1] + 1;
+            
+            if(nums[(n-1) - i] == 1)
+                suf[(n-1) - i] = suf[n - i] + 1;
         }
 
-        suf[n-1] = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            if(nums[i] == 1)
-                suf[i] = suf[i+1] + 1;
-        }
-
-        // int ans = max(pre[n-2], suf[1]);
         int ans = 0;
         for(int i=0; i<n; i++){
             int val = (i-1 >= 0 ? pre[i-1] : 0) + (i+1 < n ? suf[i+1] : 0);
