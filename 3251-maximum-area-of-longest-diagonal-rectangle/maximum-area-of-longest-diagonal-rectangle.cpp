531e058c1becf60260ad39a414f424e2da558119
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-26 06:56:05 +0530

    Time: 3 ms (19.83%) | Memory: 29.9 MB (8.78%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        int max_sq_diag = 0;
-        for(auto d : dimensions){
-            max_sq_diag = max(max_sq_diag, d[0]*d[0] + d[1]*d[1]);
-        }
+        int max_sq_diag = 0, max_area = 0;
 
-        int max_area = 0;
         for(auto d : dimensions){
             int new_sq_diag = d[0]*d[0] + d[1]*d[1];
             if(new_sq_diag == max_sq_diag){
                 max_area = max(max_area, d[0]*d[1]);
+            }
+            if(new_sq_diag > max_sq_diag){
+                max_sq_diag = new_sq_diag;
+                max_area    = d[0]*d[1];
             }
         }
 
