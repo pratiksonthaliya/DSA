fb63b28a6bacda1a403cac393b62fcfa06ada36c
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-08-29 10:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (86.22%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,37 +1,4 @@
 class Solution {
 public:
-    // long long count(int n, int m){
-
-    //     long long ans = 0;
-
-    //     for(int x=n; x>=1; x-=2){
-    //         ans += min(x, m-1);
-    //     }
-
-    //     for(int y=m-2; y>=1; y-=2){
-    //         ans += min(n-1, y);
-    //     }
-
-    //     return ans;
-
-    //     // 3, 2
-    //     // {3, 2} -> {2, 1}
-    //     // {1, 2}
-
-    //     // 5, 7
-
-    //     // {5, 7}, {3, 7}, {1, 7}
-    //     // {5, 5}, {5, 3}, {5, 1}
-
-    //     // {5, 7} -> {4, 6}, {3, 5}, {2, 4}, {1, 3}
-
-    // }
-    long long flowerGame(int n, int m) {
-        long long oddN = (n + 1) / 2;
-        long long evenN = n / 2;
-        long long oddM = (m + 1) / 2;
-        long long evenM = m / 2;
-
-        return oddN * evenM + evenN * oddM;
-    }
+    long long flowerGame(int n, int m) { return (long long)m * n / 2; }
 };
