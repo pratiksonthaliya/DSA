188662b63d1fa310dfec05d1fae89cf664a34e96
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2025-09-06 10:01:43 +0530

    Time: 58 ms (40.58%) | Memory: 183.3 MB (64.49%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,37 +1,56 @@
 class Solution {
 private:
     long long findOps(long long n){
-        long long y = 1, ops = 0;
-        for(int i=1; ; i++){
-            if(n <= 0) break;
+        if (n < 0) return 0;
+        long long ops = 0, prev = 0, k = 1;
 
-            long long lo = (1 << (i-1)) * 1LL * (1 << (i-1));
-            long long hi = (1 << i) * 1LL * (1 << i) - 1;
-            // cout << lo << " " << hi << " " << ops << " " << n << endl;
-            if(hi - lo >= n){
-                lo = 1;
-                hi = n;
-            }
-            
-            ops += (hi - lo + 1) * i;
-            n -= (hi - lo + 1);
+        while(prev < n){
 
-            // cout << lo << " " << hi << " " << ops << " " << n << endl;
+            long long next = (1LL<<k) * (1LL<<k);  // 4^k
+            // long long prev = (1LL<,(k-1)) * (1Ll<<(k-1)) // 4^(k-1)
+
+            ops += (min(next-1, n) - prev) * k;
+            prev = next-1;
+            k++;
         }
-        
-        cout << endl;
-
         return ops;
     }
 public:
     long long minOperations(vector<vector<int>>& queries) {
-        // 3 -> 12 * 2 -> 48 * 3 -> 
+        // 3 * 1 -> 12 * 2 -> 48 * 3 -> 
+
+        for(int i=0; i<17; i++){
+            cout << i << " " << findOps(i) << endl;
+        }
 
         long long ans = 0;
         for(auto &q : queries){
-            ans += (findOps(q[1]) - findOps(q[0] - 1) + 1) / 2;
+            ans += (findOps(q[1]) - findOps(q[0] - 1) + 1)/2;
         } 
 
         return ans;
     }
 };
+
+// 0 -> 1
+// 1 -> 1 -> 1
+// 2 -> 1 -> 2
+// 3 -> 1 -> 2
+
+// 4 -> 2 -> 4
+// 5 -> 2 -> 4
+// 6 -> 2 -> 6
+// 7 -> 2 -> 6
+
+// 8 -> 2 -> 8
+// 9 -> 2 -> 8
+// 10 -> 2 -> 10
+// 11 -> 2 -> 10
+
+// 12 -> 2 -> 12
+// 13 -> 2 -> 12
+// 14 -> 2 -> 14
+// 15 -> 2 -> 14
+
+// num/4 -> a
+// (a) * (a+1) / 2 * 4 + (num + 1)%4 * (a+1) 
