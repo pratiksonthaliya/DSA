28164b14732884fc731d0535c642e5975f3a2d88
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-02-07 21:26:53 +0530

    Time: 50 ms (21.60%) | Memory: 55 MB (20.86%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -3,18 +3,17 @@ public:
     int minimumDeletions(string s) {
         int n = s.size();
 
-        vector<int> pre_b(n, 0), suf_a(n, 0);
+        int a = 0;
+        vector<int> pre_b(n, 0), pre_a(n, 0);
         for(int i=0; i<n; i++){
+            if(s[i] == 'a') a++;
             pre_b[i] = (i==0 ? 0 : pre_b[i-1]) + (s[i] == 'b');
-        }
-
-        for(int i=n-1; i>=0; i--){
-            suf_a[i] = (i==n-1 ? 0 : suf_a[i+1]) + (s[i] == 'a');
+            pre_a[i] = (i==0 ? 0 : pre_a[i-1]) + (s[i] == 'a');
         }
 
         int ans = n;
         for(int i=0; i<n; i++){
-            ans = min(ans, (i==0 ? 0 : pre_b[i-1]) + (i==n-1 ? 0 : suf_a[i+1]));
+            ans = min(ans, (i==0 ? 0 : pre_b[i-1]) + (a - pre_a[i]));
         }
         return ans;
     }
