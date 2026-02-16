3973f453252169a10ea2016e8351fc7fbea00681
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-02-16 09:46:49 +0530

    Time: 2 ms (37.23%) | Memory: 8.2 MB (46.07%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        
+        int ans = 0;
+        for(int i=0; i<31; i++){
+            if((1<<i)&n)
+                ans += (1<<(31-i));
+        }
+        return ans;
+    }
+};
