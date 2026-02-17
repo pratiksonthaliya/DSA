5f1ebfbec6ea6f72da672965cf121c22679c65a2
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-02-17 19:59:22 +0530

    Time: 3 ms (9.95%) | Memory: 10.5 MB (8.12%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        map<int, set<int>> b_hr, b_min;
+        for(int hr=0; hr<12; hr++){
+            b_hr[__builtin_popcount(hr)].insert(hr);
+        }
+
+        for(int min=0; min<60; min++){
+            b_min[__builtin_popcount(min)].insert(min);
+        }
+
+        vector<string> ans;
+        for(int b=0; b<=turnedOn; b++){
+            for(int hr : b_hr[b]){
+                for(int min : b_min[turnedOn - b]){
+                    ans.push_back(to_string(hr) + ":" + 
+                        (to_string(min).size() == 1 ? "0" + to_string(min) : to_string(min)));
+                }
+            }
+        }
+
+        return ans;
+    }
+};
