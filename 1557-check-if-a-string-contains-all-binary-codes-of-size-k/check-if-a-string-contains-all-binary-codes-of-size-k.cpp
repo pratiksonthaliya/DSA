0cb81cad90a64a7f29667d5b79c38deef3a368d4
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-02-23 11:09:23 +0530

    Time: 244 ms (74.64%) | Memory: 60.4 MB (86.96%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -7,8 +7,6 @@ public:
         for(int i=k-1; i<s.size(); i++){
             int val = stoi(s.substr(i-k+1, k), nullptr, 2);
 
-            if(val > (1<<k)+1) return 0;
-
             if(!seen[val]){
                 seen[val] = 1;
                 ct++;
