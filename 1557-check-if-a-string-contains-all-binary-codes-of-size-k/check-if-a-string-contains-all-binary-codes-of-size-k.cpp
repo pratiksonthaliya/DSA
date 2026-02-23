8f516f6448e894f54c6361efef986ba258fb885d
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-02-23 11:09:15 +0530

    Time: 242 ms (74.64%) | Memory: 60.6 MB (86.77%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        set<string> st;
-        for(int i=k-1; i<s.size(); i++){
-            st.insert(s.substr(i-k+1, k));
-        }
+        int ct = 0;
+        vector<bool> seen((1<<k), 0);
 
-        return (st.size() == (1<<k));
+        for(int i=k-1; i<s.size(); i++){
+            int val = stoi(s.substr(i-k+1, k), nullptr, 2);
+
+            if(val > (1<<k)+1) return 0;
+
+            if(!seen[val]){
+                seen[val] = 1;
+                ct++;
+            }
+        }
+        return (ct == (1<<k));
     }
 };
