d94778634f9cc4f34e1cb1f4cc39ee09de177c47
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-03-02 13:26:37 +0530

    Time: 3 ms (29.27%) | Memory: 29.4 MB (98.78%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -7,19 +7,17 @@ public:
         for(int i=0; i<n; i++){
             int j = m-1;
             while(j >= 0 && grid[i][j] == 0)j--;
-
             arr.push_back(m-1 - j);
         }
 
         int op = 0;
         for(int i=0; i<n; i++){
-            if(arr[i] >= (n-1-i)) continue;
-
-            int k = (n-1 - i);
-            int mx_i = n-1, mx = arr[i];
-
+            int val = (n-1 - i);
+            if(arr[i] >= val) continue;
+            
+            int mx_i = n-1;
             for(int j=i+1; j<n; j++){
-                if(arr[j] >= k){
+                if(arr[j] >= val){
                     mx_i = j;
                     break;
                 }
@@ -29,31 +27,10 @@ public:
                 swap(arr[j], arr[j-1]);
                 op++;
             }
-        }
 
-        for(int i=0; i<n; i++){
-            cout << arr[i] << " ";
-        }
-
-        for(int i=0; i<n; i++){
-            int k = (n-1 - i);
-            if(arr[i] < k) return -1;
+            if(arr[i] < val) return -1;
         }
 
         return op;
-
-        // sort(arr.rbegin(), arr.rend());
-
-        // for(int i=0; i<n; i++){
-        //     cout << arr[i][0] << " ";
-        // }
-
-        // int ans = 0;
-        // for(int i=0; i<n; i++){
-        //     int k = (n-1 - i);
-        //     if(arr[i][0] < k) return -1;
-        //     ans += abs(i + arr[i][1]);
-        // }
-        // return ans;
     }
 };
