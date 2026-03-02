f27b4a2d9c11fbb0034d9835934554d346d7fea5
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-03-02 13:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (90.24%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -15,7 +15,7 @@ public:
             int val = (n-1 - i);
             if(arr[i] >= val) continue;
             
-            int mx_i = n-1;
+            int mx_i = -1;
             for(int j=i+1; j<n; j++){
                 if(arr[j] >= val){
                     mx_i = j;
@@ -23,12 +23,12 @@ public:
                 }
             }
 
+            if(mx_i == -1) return -1;
+
             for(int j=mx_i; j>i; j--){
                 swap(arr[j], arr[j-1]);
                 op++;
             }
-
-            if(arr[i] < val) return -1;
         }
 
         return op;
