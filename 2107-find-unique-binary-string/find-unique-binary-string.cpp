d2b9ec79e92a8148c4ef516a1a2e86a6efb482dd
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-03-08 22:24:52 +0530

    Time: 6 ms (14.87%) | Memory: 13.1 MB (35.45%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    int n;
-    unordered_set<string> numsSet;
-    
-    string generate(string curr) {
-        if (curr.size() == n) {
-            if (numsSet.find(curr) == numsSet.end()) {
-                return curr;
-            }
-            
-            return "";
-        }
-        
-        string addZero = generate(curr + "0");
-        if (addZero.size() > 0) {
-            return addZero;
-        }
-        
-        return generate(curr + "1");
-    }
-    
     string findDifferentBinaryString(vector<string>& nums) {
-        n = nums.size();
-        for (string s : nums) {
-            numsSet.insert(s);
+        int n = nums[0].size();
+        unordered_set<string> st(nums.begin(), nums.end());
+
+        for(int i=0; i<=(1<<n); i++){
+
+            cout << i << " ";
+
+            string s(n, '0');
+
+            int num = i;
+            for(int j=n-1; j>=0; j--){
+                if(num & (1<<j)) s[j] = '1';
+            }
+
+            cout << s << endl;
+
+            if(!st.count(s)) return s;
         }
-        return generate("");
+        return "";
     }
 };
