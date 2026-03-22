eabde12090ba56a099c49fd6bd472bb1c91b52fd
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-03-22 09:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (20.77%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -3,46 +3,24 @@ public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         int n = mat.size();
 
-        bool ans = true;
+        bool ans1 = true, ans2 = true, ans3 = true, ans4 = true;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(mat[i][j] != target[i][j]){
-                    ans = false;
+                if(mat[i][j] != target[i][j]){ // Direct Compare
+                    ans1 = false;
+                }
+                if(mat[i][j] != target[j][n-i-1]){ // 90 Degree Clockwise
+                    ans2 = false;
+                }
+                if(mat[i][j] != target[n-j-1][i]){ // 90 Degree Anti-Clockwise / 270 Degree Clockwise
+                    ans3 = false;
+                }
+                if(mat[i][j] != target[n-i-1][n-j-1]){ // 180 Degree Clockwise
+                    ans4 = false;
                 }
             }
         }
-        if(ans) return ans;
 
-        ans = true;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(mat[i][j] != target[j][n-i-1]){
-                    ans = false;
-                }
-            }
-        }
-        if(ans) return ans;
-
-        ans = true;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(mat[i][j] != target[n-j-1][i]){
-                    ans = false;
-                }
-            }
-        }
-        if(ans) return ans;
-
-        ans = true;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(mat[i][j] != target[n-i-1][n-j-1]){
-                    ans = false;
-                }
-            }
-        }
-        if(ans) return ans;
-
-        return false;
+        return (ans1 | ans2 | ans3 | ans4);
     }
 };
