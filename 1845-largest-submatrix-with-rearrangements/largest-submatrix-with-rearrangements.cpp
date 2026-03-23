357e412bfb8b66930e96242670d582945c660de4
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-03-23 12:56:39 +0530

    Time: 63 ms (18.77%) | Memory: 98 MB (13.18%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -6,7 +6,6 @@ public:
         int ans = 0;
         for(int i = 0; i < n; i++){
             vector<int> vals;
-
             for(int j = 0; j < m; j++){
                 if(i > 0 && mat[i][j] != 0) mat[i][j] += mat[i-1][j];
                 vals.push_back(mat[i][j]);
@@ -14,15 +13,9 @@ public:
 
             sort(vals.begin(), vals.end());
             for(int j=0; j<vals.size(); j++){
-                // cout << vals[j] << " ";
                 ans = max(ans, vals[j] * ((int)vals.size() - j));
             }
-            // cout << endl;
         }
-
         return ans;
     }
 };
-
-// 1 1 0
-// 1 0 1
