79da3c3a55ae69d0fad9d6bfd520bef9f767347b
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-04-05 17:14:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (13.45%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -3,10 +3,8 @@ public:
     bool judgeCircle(string moves) {
         int x = 0, y = 0;
         for(char &ch : moves){
-            if(ch == 'U') y++;
-            else if(ch == 'D') y--;
-            else if(ch == 'R') x++;
-            else if(ch == 'L') x--;
+            y = y + (ch == 'U') - (ch == 'D');
+            x = x + (ch == 'R') - (ch == 'L');
         }
         return (x==0 && y==0);
     }
