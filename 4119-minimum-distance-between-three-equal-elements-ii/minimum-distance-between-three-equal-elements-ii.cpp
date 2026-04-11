6ec8e76615c875bebe8423a801beea801f9c4a0d
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-04-11 11:17:23 +0530

    Time: 546 ms (29.50%) | Memory: 348.2 MB (45.50%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
@@ -9,13 +9,10 @@ public:
         int ans = INT_MAX;
         for(auto &x : mp){
             vector<int> &idxs = x.second;
-            if(idxs.size() < 3) continue;
-
             for(int i=1; i<idxs.size()-1; i++){
                 ans = min(ans, abs(idxs[i]-idxs[i-1]) + abs(idxs[i]-idxs[i+1]) + abs(idxs[i+1]-idxs[i-1]));
             }
         }
-
         return (ans == INT_MAX ? -1 : ans);
     }
 };
