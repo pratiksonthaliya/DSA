c7d094f736a9fe0fb344454e5252aed385560a5c
Pratik Sonthaliya <118986435+pratiksonthaliya@users.noreply.github.com>
2026-05-03 14:52:53 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (84.87%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -6,15 +6,12 @@ public:
         int n = s.size();
         for(int i=0; i<n; i++){
             bool isAns = true;
-            cout << i << " -> ";
             for(int j=0; j<n; j++){
-                //cout << (i+j+n)%n << " ";
                 if(goal[j] != s[(i+j+n)%n]){
                     isAns = false;
                     break;
                 }
             }
-            // cout << endl;
             if(isAns) return true;
         }
         return false;
